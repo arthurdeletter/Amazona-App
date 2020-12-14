add90beb9e70532e64cdbc36a1a9e3b7b8d44ecb
arthur de letter <arthur.deletter@hotmail.com>
2020-12-14 17:27:38 +0100

    geen idee

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Nike slim shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 10,
+            description: 'High quality shirt'
+        },
+        {
+            _id: '2',
+            name: 'Adidas fit shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100,
+            countInStock: 20,
+            brand: 'Adidas',
+            rating: 3.5,
+            numReviews: 15,
+            description: 'High quality shirt'
+        },
+        {
+            _id: '3',
+            name: 'Lacoste slim shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 140,
+            countInStock: 0,
+            brand: 'Lacoste',
+            rating: 5,
+            numReviews: 20,
+            description: 'High quality shirt'
+        },
+        {
+            _id: '4',
+            name: 'Lacoste pants',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 130.78,
+            countInStock: 15,
+            brand: 'Lacoste',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality pants'
+        },
+        {
+            _id: '5',
+            name: 'Puma slim pants',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 99.99,
+            countInStock: 5,
+            brand: 'Puma',
+            rating: 4,
+            numReviews: 15,
+            description: 'High quality pants'
+        },
+        {
+            _id: '6',
+            name: 'Adidas slim pants',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 110,
+            countInStock: 12,
+            brand: 'Adidas',
+            rating: 5,
+            numReviews: 10,
+            description: 'High quality pants'
+        },
+    ]
+}
+
+export default data;
